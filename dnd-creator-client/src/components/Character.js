b7a6c3c4a60c6b08ae0503a06b0b2d2d89689866
John Martin <jmart07@gmail.com>
2020-02-08 08:19:17 -0500

    front end running

---
+++ dnd-creator-client/src/components/Character.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Character extends Component {
+  render() {
+    return(
+      <>
+        This is a test
+      </>
+    );
+  }
+}
+
+export default Character;
